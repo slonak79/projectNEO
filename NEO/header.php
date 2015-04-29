5602da41776f45886ed5e665005299bb7f9e99b3
Hector Sandoval <hesandova@gmail.com>
2015-04-29 13:35:42 -0700

    Added dashboard stuff need to update fields

--- NEO/header.php
+++ NEO/header.php
@@ -9,7 +9,6 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>login</title>
     <meta name="description" content="">
     <meta name="author" content="Devon">
 
