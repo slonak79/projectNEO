45401dff01f6db120a44934ae491e29a0a6f8ef6
artLopez <arlopez@csumb.edu>
2015-04-29 15:08:17 -0700

    Finshed creating the form to create evacuess

--- NEO/createEvacuee.php
+++ NEO/createEvacuee.php
@@ -1,31 +1,22 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: arturolopez
- * Date: 4/29/15
- * Time: 10:28 AM
-*/
-
-?>
 <!DOCTYPE html>
 <html>
 <?php require "header.php"; ?>
 
 <body>
-    <?php require "navbar.php" ?>
+    <?php require "navbar.php"; ?>
 
     <h1>Create Evacuee</h1>
 
     <div align="center">
 
-            <form method = "POST" action = "signUp.php" >
+            <form action = "signUp.php"  method = "POST" >
                 <fieldset class="scheduler-border">
                     <legend class="scheduler-border">Registration</legend>
 
                     <label>Surname:</label> <input type = "text" name = "surname"><br>
                     <label>First name:</label>  <input type = "text" name = "givenName"><br>
                     <label>Nationality:</label> <input type = "text" name = "nation"><br>
-                    <label>Date of Birth:</label>  <input type="date" name="bday"><br>
+                    <label>Date of Birth:</label>  <input type="date" name="birthday"><br>
                     <label>Place of Birth: </label><input type = "text" name="placeBirth"><br>
                     <label>Date of issue: </label> <input type="date" name = "issue"><br>
                     <label>Date of Expiration: </label><input type = "date" name = "dateExpiration"><br>
--- NEO/signUp.php
+++ NEO/signUp.php
@@ -1,3 +1,4 @@
+<script>setTimeout(function(){window.location.href='index.php'},5000);</script>
 <?php
 /**
  * Created by PhpStorm.
@@ -9,25 +10,32 @@
 require 'mysqlConfig.php';
 $dbConn = getConnection();
 
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if(isset($_POST['birthday'])){
 
-$sql = "INSERT INTO evacuee (`surname`,`given_name`,`nationality`, `date_of_birth`, `place_of_birth`, `date_of_issue`, `date_of_expiration`,
-         `sex`, `authority`) VALUES (':surname', ':given_name', ':nationality',':date_of_birth',':place_of_birth',':date_of_issue',
-         ':date_of_expiration',':sex',':authority')";
+        $sql = "INSERT INTO evacuee (`surname`,`given_name`,`nationality`, `date_of_birth`, `place_of_birth`, `date_of_issue`, `date_of_expiration`,
+         `sex`, `authority`) VALUES (:surname, :given_name, :nationality,:date_of_birth,:place_of_birth,:date_of_issue,
+         :date_of_expiration,:sex,:authority)";
 
-$namedParameters = array();
-$namedParameters[":surname"]    = $_POST['surname'];
-$namedParameters[":given_name"] = $_POST['givenName'];
-$namedParameters[":nationality"] = $_POST['nation'];
-$namedParameters[":date_of_birth"] = $_POST['bday'];
-$namedParameters[":place_of_birth"] = $_POST['placeBirth'];
-$namedParameters[":date_of_issue"] = $_POST['issue'];
-$namedParameters[":date_of_expiration"] = $_POST['dateExpiration'];
-$namedParameters[":sex"] = $_POST['gender'];
-$namedParameters[":authority"] = $_POST['authority'];
+        $namedParameters = array();
+        $namedParameters[":surname"]        = $_POST['surname'];
+        $namedParameters[":given_name"]     = $_POST['givenName'];
+        $namedParameters[":nationality"]    = $_POST['nation'];
+        $namedParameters[":date_of_birth"]  = $_POST['birthday'];
+        $namedParameters[":place_of_birth"] = $_POST['placeBirth'];
+        $namedParameters[":date_of_issue"]  = $_POST['issue'];
+        $namedParameters[":date_of_expiration"] = $_POST['dateExpiration'];
+        $namedParameters[":sex"]             = $_POST['gender'];
+        $namedParameters[":authority"]       = $_POST['authority'];
 
-$stmt = $dbConn->prepare($sql);
-$stmt -> execute($namedParameters);
-$results = $stmt->fetch();
+        $stmt = $dbConn->prepare($sql);
+        $stmt -> execute($namedParameters);
 
+        echo "<h1>The evacuee information was successfully stored!</h1>";
+
+    }else{
+        echo "not set";
+    }
+}
 
 ?>
