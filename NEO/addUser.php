cc72144475c8f52f88cd101fd22cad4ee4f6e346
Hector Sandoval <hesandova@gmail.com>
2015-05-03 21:31:58 -0700

    Added js file for ajax call to fill select tag

--- NEO/addUser.php
+++ NEO/addUser.php
@@ -18,19 +18,46 @@
         <?php require "taskPanel.php"?>
         <div align="center">
             <h1>Create User</h1>
-            <form action = "signUpUser.php"  method = "POST" >
+            <form action="signUpUser.php" class="form-horizontal" method="POST" id="addUserForm" name="addUserForm">
                 <fieldset class="scheduler-border">
                     <legend class="scheduler-border">Registration</legend>
-                    <label for="username">Username:</label> <input type="text" name="username" id="username"required><br>
-                    <label for="password">Password:</label> <input type="text" name="password" id="password" required><br>
-                    <label for="first_name">First Name:</label>  <input type="text" name="first_name" id="first_name" required><br>
-                    <label for="last_name">Last Name:</label> <input type="text" name="last_name" id="last_name" required><br>
-                    <label for="email">Email:</label>  <input type="text" name="email" id="email" required><br>
-                    <label for="phone_number">Phone Number: </label><input type="tel" name="phone_number" id="phone_number" required><br>
-                    <label for="profile_picture">Profile Picture: </label><input type="file" name="profile_picture" id="profile_picture" required/> <br>
-                    <label>Sex:</label>
-                    <input type = "radio" name="gender" value ="M" id="male_radio"> <label for="male_radio">Male</label>
-                    <input type = "radio" name="gender" value ="F" id="female_radio"><label for="female_radio">Female</label> <br>
+                    <div class="form-group">
+                        <label for="username" class="control-label">Username:</label>
+                        <input type="text" name="username" id="username"required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="control-label">Password:</label>
+                        <input type="text" name="password" id="password" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="first_name" class="control-label">First Name:</label>
+                        <input type="text" name="first_name" id="first_name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="last_name" class="control-label">Last Name:</label>
+                        <input type="text" name="last_name" id="last_name" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="control-label">Email:</label>
+                        <input type="text" name="email" id="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="phone_number" class="control-label">Phone Number: </label>
+                        <input type="tel" name="phone_number" id="phone_number" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="profile_picture" class="control-label">Profile Picture: </label>
+                        <input type="file" name="profile_picture" id="profile_picture" required/>
+                    </div>
+                    <div class="form-group">
+                        <select form="addUserForm" class="form-control" id="roles_select">
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label>Sex:</label>
+                        <input type = "radio" name="gender" value ="M" id="male_radio"> <label for="male_radio">Male</label>
+                        <input type = "radio" name="gender" value ="F" id="female_radio"><label for="female_radio">Female</label>
+                    </div>
                     <button type="submit" class="btn btn-success">
                         <i class="icon-circle-arrow-right icon-large"></i> Submit
                     </button>
---
+++ NEO/js/loadRoles.js
@@ -0,0 +1,28 @@
+/**
+ * Created by edsan on 5/3/15.
+ */
+
+$.ajax({
+    url:"adminFunctions.php",
+    type:"GET",
+    data: {
+        action:"roles"
+    },
+    success: function (json) {
+        $.each($.parseJSON(json),function(key, value){
+            $("#roles_select").append($("<option></option>")
+                .attr("value",key)
+                .text(value));
+        });
+    },
+    error: function (xhr, status, errorThrown) {
+        //alert( "Sorry, there was a problem!" );
+        console.log( "Error: " + errorThrown );
+        console.log( "Status: " + status );
+        console.dir( xhr );
+    },
+    complete: function( xhr, status ) {
+        //alert( "The request is complete!" );
+    }
+});
+
